3e096aed6948b2b0d87c3692cfa39ac9ababa65e
JonDm07 <jan.drmota1@gmail.com>
2023-03-08 12:18:39 +0100

    Add game board module and game controller module
    Added an array representing the board, added game controller that checks
    the rows and columns everytime a player plays. It will not check
    diagonally

--- script.js
+++ script.js
@@ -1,28 +1,93 @@
-const playerFactory = (name) => {
-    return {name}
+const gameBoard = (() => {
+    let gameBoardArr = [
+        ["C1","C2","C3"],
+        ["C4","C5","C6"],
+        ["C7","C8","C9"]
+    ]
+
+    return {gameBoardArr}
+
+})();
+
+function player(name) {
+    function play(x, y, symbol) {
+        if (gameBoard.gameBoardArr[x][y] !== "O" && gameBoard.gameBoardArr[x][y] !== "X") {
+            gameBoard.gameBoardArr[x][y] = symbol
+            evaluateRow()
+            evaluateColumn()
+        }
+    }
+
+    return {name, play}
 }
 
-(function gameBoard() {
-    const arr = Array.from(document.querySelectorAll(".game-tile"))
+const player1 = player("J")
 
-    let turn = 0
-    
-    arr.forEach(div => {
-        div.addEventListener("click", () => {
-            if(turn % 2 === 0) {
-                div.style.backgroundColor = "green"
-                turn++
-                console.log(`${turn} next is red`)
-            } else {
-                div.style.backgroundColor = "red"
-                turn++
-                console.log(`${turn} next is green`)
+
+
+const gameController = (() => {
+    const evaluateRow = function() {
+        for(let i = 0; i < 3; i++) {
+
+            let arrX = []
+            let arrO = []
+
+            for(let j = 0; j < 3; j++) {
+                if(gameBoard.gameBoardArr[i][j] === "X") {
+                    arrX.push(gameBoard.gameBoardArr[i][j])
+                } else if (gameBoard.gameBoardArr[i][j] === "O") {
+                    arrO.push(gameBoard.gameBoardArr[i][j])
+                }                
             }
 
-        })
-    })
+            if(arrX.length === 3 || arrO.length === 3) {
+                alert(`WTF it happened X:${arrX.length} O:${arrO.length}`)
+            }
+        }
+    }
+
+    const evaluateColumn = function() {
+        for(let i = 0; i < 3; i++) {
+
+            let arrX = []
+            let arrO = []
+
+            for(let j = 0; j < 3; j++) {
+                if(gameBoard.gameBoardArr[j][i] === "X") {
+                    arrX.push(gameBoard.gameBoardArr[j][i])
+                } else if (gameBoard.gameBoardArr[j][i] === "O") {
+                    arrO.push(gameBoard.gameBoardArr[j][i])
+                }                
+            }
+
+            if(arrX.length === 3 || arrO.length === 3) {
+                alert(`WTF it happened X:${arrX.length} O:${arrO.length}`)
+            }
+
+            console.log(arrX)
+            console.log(arrO)
+            console.log(gameBoard.gameBoardArr)
+        }
+
+    }
+
+/*     const play1 = (x, y) => {
+        if (gameBoard.gameBoardArr[x][y] !== "O" && gameBoard.gameBoardArr[x][y] !== "X") {
+            gameBoard.gameBoardArr[x][y] = "X"
+            evaluateRow()
+            evaluateColumn()
+        }
+    }
+    const play2 = (x, y) => {
+        if (gameBoard.gameBoardArr[x][y] !== "O" && gameBoard.gameBoardArr[x][y] !== "X") {
+            gameBoard.gameBoardArr[x][y] = "O"
+            evaluateRow()
+            evaluateColumn()
+        }
+    } */
+
+    return {evaluateRow, evaluateColumn, play1, play2}
+
 })();
 
-(function gameController() {
 
-})();
